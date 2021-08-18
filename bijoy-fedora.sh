efad84e4a2b10cad389376860c3214739c2bff60
sha314 <shahnoor3pl@gmail.com>
2021-08-19 00:11:11 +0600

    mim and png files added. and fedora script

---
+++ bijoy-fedora.sh
@@ -0,0 +1,27 @@
+
+# Install dbus plugin
+
+sudo dnf install ibus-m17n
+
+
+# copy bijoy layout and icons 
+sudo cp ./m17n-files/*.mim /usr/share/m17n/
+sudo cp ./m17n-files/*.png /usr/share/m17n/icons/
+
+# Now reboot/restart your pc
+
+# Go to settings > Keyboard > Input Sources
+
+# Click plus icon or add button
+
+# Click More button
+
+# Type bijoy
+
+# Go to Bangla (Bangladesh)
+
+# Select Bangla (bijoyClassic (m17n)) and click add
+# Select Bangla (bijoyUnicode (m17n)) and click add
+
+# Enjoy
+
